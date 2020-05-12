f29a0967ad0568e2bd6eb8fb624a142446dad0a9
= <=>
2020-05-12 23:44:32 +0700

    Change login layout

--- src/component/Login/index.js
+++ src/component/Login/index.js
@@ -1,30 +1,90 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './../../style/global.css';
 import './index.scss';
 
-import LoginInputForm from './../LoginInputForm/index';
+import { Button, Input } from 'antd';
 
 class Login extends Component {
   constructor(props){
     super(props)
+
+    this.state = {
+      userName: "",
+      password: "",
+    }
+  }
+
+  handleChange = (type, value) => {
+    if (type == 'username'){
+      this.setState({ userName: value.currentTarget.value});
+    }
+    if (type == 'password'){
+      this.setState({ password: value.currentTarget.value});
+    }
+  }
+
+  handleClickSignInButton = () => {
+    const { userName, password } = this.state;
+    
+    let params = {
+      username: userName,
+      password: password,
+    }
+  }
+
+  renderInputForm(type) {
+    const { userName, password } = this.state;
+
+    let label = type == 'password'? "Password":"Username";
+    let defaultValue = type == 'password'? password : userName;
+
+    return (
+      <div>
+        <div> {label} </div>
+        <Input
+          onChange={(value) => this.handleChange(type, value)}
+          defaultValue={defaultValue}
+        />
+      </div>
+    )
   }
 
   render(){
     return(
-      <body className="background">
+      <div className="background">
         <div className="login_wrapper">
-          <LoginInputForm
+          {/* <LoginInputForm
             defaultValue={"Name"}
           />
           <LoginInputForm
             defaultValue={"Password"}
             password={true}
-          />
+          /> */}
+          {this.renderInputForm('username')}
+          {this.renderInputForm('password')}
         </div>
-      </body>
+        <Button
+          size="small"
+          placeholder="Log in"
+          onClick={() => this.handleClickSignInButton()}
+          className="style-button"
+        />
+      </div>
       
     )
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth,
+  }
+}
  
-export default Login;
+export default connect(
+  mapStateToProps,
+  {
+
+  }
+)(Login);
